d50a9aa29403c4fd74439df64a7f43f97aaed8ea
Dominik Dosoudil <dosoudil@insoft.cz>
2018-01-14 02:39:17 +0100

    Prepare discovery part of handshake

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use std::collections::HashMap;
+#[macro_use]
+extern crate serde_derive;
 
+extern crate serde;
+extern crate serde_json;
 
 mod server;
 
+use std::collections::HashMap;
 use server::ServerProtocol;
-
+use serde_json::to_string;
 struct Protocol {}
 
 impl ServerProtocol for Protocol {
-	fn handle_request(&self, request: server::Request) -> server::Result {
+	fn handle_request(&self, request: server::Request) -> server::Response {
 		let mut body = HashMap::new();
 		body.insert("content".to_string(), "Hello fucking world".to_string());
-		server::Result {
+		server::Response {
 			header: String::from("MESSAGE"),
 			body: body,
 		}
@@ -19,8 +23,14 @@ impl ServerProtocol for Protocol {
 }
 
 fn main() {
-	let srv = server::Server::new();
+	let mut srv = server::Server::new();
 	srv.shake();
-	srv.listen(p);
 
+	/*
+	let mut map: HashMap<String, String> = HashMap::new();
+
+	map.insert("a".to_string(), "b".to_string());
+
+	println!("{:?}", to_string(&map));
+	*/
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,10 @@
 use std::net::UdpSocket;
 //use std::{thread, str, time};
 use std::collections::HashMap;
+use std::time::Duration;
+use std::str;
+
+use serde_json::{to_string as json, from_str as from_json};
 
 enum States {
 	Init,
@@ -16,19 +20,26 @@ enum Roles {
 	King,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Request {
 	pub header: String,
 	pub body: HashMap<String, String>,
 }
 
-#[derive(Debug)]
-pub struct Result {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Response {
 	pub header: String,
 	pub body: HashMap<String, String>,
 }
 
+#[derive(Debug)]
+struct Channel {
+	pub knight: String,
+	pub king: String,
+}
+
 pub trait ServerProtocol {
-	fn handle_request(&self, request: Request) -> Result;
+	fn handle_request(&self, request: Request) -> Response;
 }
 
 
@@ -38,6 +49,7 @@ pub struct Server {
 	role: Roles,
 	socket: UdpSocket,
 	queue: Vec<Request>,
+	channels: HashMap<String,Channel>,
 }
 
 impl Server {
@@ -51,21 +63,55 @@ impl Server {
 			role: Roles::Slave,
 			socket: sock,
 			queue: Vec::new(),
+			channels: HashMap::new(),
 		}
 	}
 
-	pub fn shake(&self) {
+	fn add_channel(&mut self, name: String, king: String) {
+		&self.channels.insert(name, Channel {
+			king: king,
+			knight: String::new(),
+		});
+	}
 
+	fn discover(&self) {
+	}
+
+	pub fn shake(&mut self) {
+		let mut buff = [0; 255];
+		let body = HashMap::new();
+		let mut discover_msg = Response {
+			header: "DISCOVER".to_owned(),
+			body: body,
+		};
+
+		self.socket.send_to(json(&discover_msg).unwrap().as_bytes(), "192.168.1.255:12345");
+		self.socket.set_read_timeout(Some(Duration::new(10, 0)));
+		let mut channels = vec![0];
+		self.socket.set_broadcast(true).expect("Couldn't set broadcast on socket.");
+		loop {
+			match self.socket.recv_from(&mut buff) {
+				Ok(t) => {
+					let (bytes_n, src_addr) = t;
+					let request: Request = from_json(str::from_utf8(&buff[0..bytes_n]).unwrap()).unwrap();
+					match &request.header[..] {
+						"HEREIAM" => {
+							&self.add_channel(request.body.get("name").unwrap().to_string(), src_addr.to_string());
+						},
+						_ => {
+							println!("Useless message");
+						}
+					}
+					//println!("Data recieved ({}) from {}: {:?}", bytes_n, src_addr, request.header);
+				},
+				Err(e) => {
+					break;
+				}
+			}
+		}
+		println!("{:?}", &self.channels);
 	}
 
 	pub fn listen<S>(&self, p: S) {
-		/*
-		let mut buff = [0; 255];
-		loop {
-			println!("Opening recv");
-			let (bytes_n, src_addr) = socket.recv_from(&mut buff).expect("Didn't rcv data");
-			println!("Data recieved ({}) from {}: {:?}", bytes_n, src_addr, str::from_utf8(&buff[0..bytes_n]));
-		};
-		*/
 	}
 }
